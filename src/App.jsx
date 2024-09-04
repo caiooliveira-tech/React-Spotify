acc25c4a9eb8beaee0835936dad75048d31204e2
Caio Oliveira <caio.oliveira07@gmail.com>
2024-09-04 01:38:30 -0300

    Cor de fundo do card randômica implementada

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,18 @@ import Card from "./components/Card"
 
 function App() {
 
+  const generateRandomColor = () => {
+    const letters = '0123456789ABCDEF';
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+  }
+
+  const randomColor = generateRandomColor();
+  
+
   return (
     <>
       <header className="bg-red-600 w-full h-28"></header>
@@ -12,15 +24,15 @@ function App() {
           <div className="bg-red-500 w-32 h-32"></div>  
         </aside>
         <div className="bg-gray-400 w-3/4 grid grid-cols-3 pl-20 items-center">
-          <Card titulo="teste" bgColor="blue" capa="capa.jpg"/>
-          <Card titulo="teste" bgColor="blue" capa="capa.jpg"/>
-          <Card titulo="teste" bgColor="blue" capa="capa.jpg"/>
-          <Card titulo="teste" bgColor="blue" capa="capa.jpg"/>
-          <Card titulo="teste" bgColor="blue" capa="capa.jpg"/>
-          <Card titulo="teste" bgColor="blue" capa="capa.jpg"/>
-          <Card titulo="teste" bgColor="blue" capa="capa.jpg"/>
-          <Card titulo="teste" bgColor="blue" capa="capa.jpg"/>
-          <Card titulo="teste" bgColor="blue" capa="capa.jpg"/>
+          <Card titulo="teste" bgColor={randomColor} capa="capa.jpg"/>
+          <Card titulo="teste" bgColor={randomColor} capa="capa.jpg"/>
+          <Card titulo="teste" bgColor={randomColor} capa="capa.jpg"/>
+          <Card titulo="teste" bgColor={randomColor} capa="capa.jpg"/>
+          <Card titulo="teste" bgColor={randomColor} capa="capa.jpg"/>
+          <Card titulo="teste" bgColor={randomColor} capa="capa.jpg"/>
+          <Card titulo="teste" bgColor={randomColor} capa="capa.jpg"/>
+          <Card titulo="teste" bgColor={randomColor} capa="capa.jpg"/>
+          <Card titulo="teste" bgColor={randomColor} capa="capa.jpg"/>
         </div>
       </section>
     </>
