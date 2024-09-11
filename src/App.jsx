678baa02aab3ce15ea6c1eb2490f10fa138be868
Caio Oliveira <caio.oliveira07@gmail.com>
2024-09-11 01:29:23 -0300

    ajuste de altura e slice do array

--- src/App.jsx
+++ src/App.jsx
@@ -16,14 +16,14 @@ function App() {
   return (
     <>
       <header className="bg-red-600 w-full h-28"></header>
-      <section className="w-full h-auto flex">
+      <section className="w-full h-auto calc min-h-[calc(100vh-112px)] flex">
         <aside className="bg-green-400 w-1/4 flex flex-col justify-around items-center">
           <div className="bg-red-500 w-32 h-32"></div>
           <div className="bg-red-500 w-32 h-32"></div>
           <div className="bg-red-500 w-32 h-32"></div>  
         </aside>
         <div className="bg-gray-400 w-3/4 grid grid-cols-3 pl-20 items-center">
-        {artistas.map(artista => (
+        {artistas.slice(0,5).map(artista => (
           <Card 
             key={artista.id} 
             titulo={artista.name}
