9b65e553cc662eae01c2372c7cde698697c631db
Caio Oliveira <caio.oliveira07@gmail.com>
2024-09-11 01:19:44 -0300

    Conexão com MongoDB funcionando

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,36 @@
+import { useEffect, useState } from "react";
 import Card from "./components/Card"
 
 function App() {
 
-  const generateRandomColor = () => {
-    const letters = '0123456789ABCDEF';
-    let color = '#';
-    for (let i = 0; i < 6; i++) {
-      color += letters[Math.floor(Math.random() * 16)];
-    }
-    return color;
-  }
+  const [artistas, setArtistas] = useState([]);
 
-  const randomColor = generateRandomColor();
   
+  useEffect(() => {
+    fetch('http://localhost:3000/artistas')
+    .then(res => res.json())
+    .then(data => setArtistas(data))
+    .catch(err => console.log(err))
+  },[])
 
   return (
     <>
       <header className="bg-red-600 w-full h-28"></header>
-      <section className="w-full h-dvh flex">
+      <section className="w-full h-auto flex">
         <aside className="bg-green-400 w-1/4 flex flex-col justify-around items-center">
           <div className="bg-red-500 w-32 h-32"></div>
           <div className="bg-red-500 w-32 h-32"></div>
           <div className="bg-red-500 w-32 h-32"></div>  
         </aside>
         <div className="bg-gray-400 w-3/4 grid grid-cols-3 pl-20 items-center">
-          <Card titulo="teste" bgColor={randomColor} capa="capa.jpg"/>
-          <Card titulo="teste" bgColor={randomColor} capa="capa.jpg"/>
-          <Card titulo="teste" bgColor={randomColor} capa="capa.jpg"/>
-          <Card titulo="teste" bgColor={randomColor} capa="capa.jpg"/>
-          <Card titulo="teste" bgColor={randomColor} capa="capa.jpg"/>
-          <Card titulo="teste" bgColor={randomColor} capa="capa.jpg"/>
-          <Card titulo="teste" bgColor={randomColor} capa="capa.jpg"/>
-          <Card titulo="teste" bgColor={randomColor} capa="capa.jpg"/>
-          <Card titulo="teste" bgColor={randomColor} capa="capa.jpg"/>
+        {artistas.map(artista => (
+          <Card 
+            key={artista.id} 
+            titulo={artista.name}
+            capa={artista.image}/>
+        ))}
         </div>
+
       </section>
     </>
   )
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,9 +1,14 @@
 /* eslint-disable react/prop-types */
-function Card({titulo, bgColor, capa}) {
+import { generateRandomColor } from "../services/card-services";
+
+function Card({titulo, capa}) {
+    
+    const randomColor = generateRandomColor();
+
     return ( 
-        <div className="w-36 h-36 flex flex-col justify-start items-center pt-3 relative rounded-md overflow-hidden" style={{backgroundColor: bgColor}}>
+        <div className="w-36 h-36 m-5 flex flex-col justify-start items-center pt-3 relative rounded-md overflow-hidden" style={{backgroundColor: randomColor}}>
             <h1 className="w-3/4 h-7 text-white font-bold">{titulo}</h1>
-            <img src={capa} alt="Capa do Album" className="w-2/3 h-2/3 absolute bottom-0 -right-2 rounded-sm rotate-[25deg] translate-x-[18%] translate-y-[-2%]" />
+            <img src={capa === "capa.jpg"? capa : "capa.jpg"} alt="Capa do Album" className="w-2/3 h-2/3 absolute bottom-0 -right-2 rounded-sm rotate-[25deg] translate-x-[18%] translate-y-[-2%]" />
         </div>
      );
 }
